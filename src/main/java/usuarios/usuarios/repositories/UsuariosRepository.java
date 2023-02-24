f9a831258bb5caf5d78ddc8d233b13182fa2d364
Fernando Rey Caires <fernando.rey.caires@gmail.com>
2023-02-24 08:22:46 -0300

    Removendo codigos não usados

--- src/main/java/usuarios/usuarios/repositories/UsuariosRepository.java
+++ src/main/java/usuarios/usuarios/repositories/UsuariosRepository.java
@@ -5,7 +5,6 @@ import org.springframework.data.repository.CrudRepository;
 import org.springframework.stereotype.Repository;
 import usuarios.usuarios.models.UsuariosModel;
 
-import java.util.List;
 import java.util.Optional;
 import java.util.UUID;
 
